72e8095d0ad0d5a8ee54b04ac26402055f8e0981
6pa4o <avisloguzov@gmail.com>
2018-02-26 14:52:00 +0300

    first commit

--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
 require_once "../vendor/autoload.php";
+require_once "../app/components/Database.php";
 use twig\twig;
+
+//$db = Database::getInstance();
+//$mysqli = $db->getConnection();
+//$sql_query = "SELECT * FROM images";
+//$result = $mysqli->query($sql_query);
+//$array_result = [];
+//while ($row = mysqli_fetch_assoc($result)) {
+//    $array_result += [
+//        "img$row[ID]" => $row
+//    ];
+//};
+//echo "<pre>";
+//print_r($array_result);
+//echo "</pre>";
+
+$loader = new Twig_Loader_Filesystem('../app/templates');
+$twig = new Twig_Environment($loader);
+$template = $twig->load('bases.tmpl');
+echo $template->render([
+    //'data' => $array_result
+]);
