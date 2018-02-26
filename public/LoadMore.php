21b4e804d6e687f22e544b1fed1d8ed336198189
6pa4o <avisloguzov@gmail.com>
2018-02-27 01:33:54 +0300

    beta version

--- public/LoadMore.php
+++ public/LoadMore.php
@@ -1,11 +1,10 @@
 <?php
 require_once "../vendor/autoload.php";
 require_once "../app/components/Database.php";
-use twig\twig;
-
+$startFrom = $_POST['startFrom'];
 $db = Database::getInstance();
 $mysqli = $db->getConnection();
-$sql_query = "SELECT * FROM goods ORDER BY 'id_good' DESC LIMIT 9";
+$sql_query = "SELECT * FROM goods ORDER BY 'id_good' DESC LIMIT {$startFrom}, 9";
 $result = $mysqli->query($sql_query);
 $array_result = [];
 while ($row = mysqli_fetch_assoc($result)) {
@@ -13,13 +12,10 @@ while ($row = mysqli_fetch_assoc($result)) {
         "good$row[id_good]" => $row
     ];
 };
-//echo "<pre>";
-//print_r($array_result);
-//echo "</pre>";
 
 $loader = new Twig_Loader_Filesystem('../app/templates');
 $twig = new Twig_Environment($loader);
-$template = $twig->load('bases.tmpl');
+$template = $twig->load('new-product.tmpl');
 echo $template->render([
     'data' => $array_result
 ]);
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,4 +6,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			document.querySelector( ".menu" )
 				.classList.toggle( "active" );
 		});
+	//load goods per click
+	var startFrom = 9;
+
+    $( '#loadmore' ).click( function() {
+        $.ajax({
+            // url: 'test.json',
+            url: 'LoadMore.php',
+            type: 'POST',
+			dataType: 'html',
+            data: {"startFrom": startFrom},
+            success: function(data){
+                if(data.length > 0){
+                $('#loadmore').before(data);
+                startFrom += 9;
+                console.log(data.length)
+                } else {
+                    $('#loadmore').hide();
+                }
+            }
+		});
+    });
 });
